be20ef2a4e83c3c06a9c6f93e2b9a9ec79097d09
Rachel Wey <rachelwey96@outlook.com>
2018-10-25 07:23:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
-var recipes = {eggs: 3}
+var recipes = {}
 
-function updateObjectWithKeyAndValue(object, key, value){
-  var newObj = Object.assign(object, {[key]:value})
-  return newObj
-  //returns original key value pairs and new key value pair
-  //doesn't modify original "recipes" object, returns clone with new data
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value})
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
-  //modifies original object with new key and value
 }
 
-function deleteFromObjectByKey(object, key){
-  var newObj = object
-  delete newObj.key
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object)
+  delete newObj[key]
   return newObj
-
 }
 
-function destructivelyDeleteFromObjectByKey(object, key){
-  delete object.key
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
   return object
 }
